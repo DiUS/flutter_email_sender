d0ec3eca6d24f7dfdf14ac4206bdc72c4f9e933f
Joo Aun Saw <jasaw@dius.com.au>
2020-06-05 11:46:22 +1000

    fix missing flutter_email_sender-Swift.h file

--- ios/Classes/FlutterEmailSenderPlugin.m
+++ ios/Classes/FlutterEmailSenderPlugin.m
@@ -1,5 +1,5 @@
 #import "FlutterEmailSenderPlugin.h"
-#import <flutter_email_sender/flutter_email_sender-Swift.h>
+#import "flutter_email_sender-Swift.h"
 
 @implementation FlutterEmailSenderPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
